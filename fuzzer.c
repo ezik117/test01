589912a3d617c5a19755946ec36b2ee9177ad282
ezik117 <eziksoft@yandex.ru>
2024-03-27 00:13:17 +0300

    fuzzer added

---
+++ fuzzer.c
@@ -0,0 +1,18 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <test.h>
+
+
+int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+  char *buf = malloc(size+1);
+  memset(buf, 0, size + 1);
+  memcpy(buf, data, size);
+    
+  count_letters(buf);
+  
+  free(buf);
+  
+  return 0;
+}
