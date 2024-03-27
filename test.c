d0b41339f845c72e99509692898697a11c2b8681
ezik117 <eziksoft@yandex.ru>
2024-03-27 07:00:13 +0300

    4

--- test.c
+++ test.c
@@ -2,7 +2,8 @@
 #include <stdint.h>
 #include "test.h"
 
-int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
     return 0;
 }
 
--- test.h
+++ test.h
@@ -1,6 +1,7 @@
 #ifndef TEST_H
 #define TEST_H
 
+int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size);
 int count_letters(char *s);
 int another_test(char *s);
 int do_not_called(char *s);
