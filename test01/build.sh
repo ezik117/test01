36d4583d6058147ddd97ae6d19bb7261c9c03ea0
ezik117 <strange_ezik@mail.ru>
2024-03-29 09:47:55 +0000

    1

--- test01/build.sh
+++ test01/build.sh
@@ -1,4 +1,9 @@
+# Build project using the OSS-Fuzz enviroonment flags.
 $CC $CFLAGS -c test.c -o test.o
+
+# Build the fuzzers. We must ensure we link with $CXX to support Centipede.
+# Fuzzers must be placed in $OUT/
 $CC $CFLAGS -c fuzzer.c -o fuzzer.o
-#$CC $CFLAGS $LIB_FUZZING_ENGINE fuzzer.o test.o -o $OUT/test01
 $CXX $CXXFLAGS $LIB_FUZZING_ENGINE fuzzer.o test.o -o $OUT/test01
+
+
